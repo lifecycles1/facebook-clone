95cd84c81ea0bdc6c0ac0b4e911b4a144f9cb647
lifecycles1 <raichev89@gmail.com>
2022-11-09 05:24:37 +0000

    re update

--- components/Login.js
+++ components/Login.js
@@ -4,16 +4,8 @@ import { signIn } from "next-auth/react";
 function Login() {
   return (
     <div className="grid place-items-center pt-20 space-y-20">
-      <Image
-        src="https://i.imgur.com/EVegH6k.png"
-        height={200}
-        width={200}
-        objectFit="contain"
-      />
-      <h1
-        onClick={signIn}
-        className="p-5 bg-blue-500 rounded-full text-white cursor-pointer"
-      >
+      <Image src="https://i.imgur.com/EVegH6k.png" height={200} width={200} objectFit="contain" alt="" />
+      <h1 onClick={signIn} className="p-5 bg-blue-500 rounded-full text-white cursor-pointer">
         Login with Facebook
       </h1>
     </div>
